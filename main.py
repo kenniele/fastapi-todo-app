28e141cb1e52af2d2353c64bedf7bb92d4c2aa31
kenniele <kenniele@yandex.com>
2024-06-16 20:31:36 +0300

    111

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, applications
+from fastapi.openapi.docs import get_swagger_ui_html
+
+
+from contextlib import asynccontextmanager
+
+from database import create_tables, delete_tables
+from router import router as tasks_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("База очищена")
+    await create_tables()
+    print("База готова к работе")
+    yield
+    print("Выключение приложения!")
+
+
+def swagger_monkey_patch(*args, **kwargs):
+    return get_swagger_ui_html(
+        *args, **kwargs,
+        swagger_js_url="https://cdn.staticfile.net/swagger-ui/5.1.0/swagger-ui-bundle.min.js",
+        swagger_css_url="https://cdn.staticfile.net/swagger-ui/5.1.0/swagger-ui.min.css")
+
+
+applications.get_swagger_ui_html = swagger_monkey_patch
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
